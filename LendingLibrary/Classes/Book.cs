e7761f96539c369f49e92103676575cca82f4c7f
Jon Rice <revyolution1120@gmail.com>
2019-06-19 21:56:22 -0700

    Add project files.

---
+++ LendingLibrary/Classes/Book.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LendingLibrary.Classes
+{
+    class Book
+    {
+    }
+}
---
+++ LibraryTests/UnitTest1.cs
@@ -0,0 +1,59 @@
+using System;
+using Xunit;
+
+namespace LibraryTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CanAddBook()
+        {
+
+        }
+
+        [Fact]
+        public void CanRemoveBook()
+        {
+
+        }
+
+        [Fact]
+        public void CannotRemoveNonExistantBook()
+        {
+
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        public void CountOfBooks()
+        {
+
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        public void BookProperties()
+        {
+
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        public void AuthorProperties()
+        {
+
+        }
+    }
+}
